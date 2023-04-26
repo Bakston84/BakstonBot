34697a7ab9b16f139fc85dc999e41c8b9f513100
Bakston84 <Bakston1@yandex.ru>
2023-04-26 10:10:45 +0500

    BB_26042023_a5

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from aiogram import executor
 from loader import dp
 
 async def on_start(_):
-    try:
-        print('DB connection... OK!')
-    except:
-        print('DB connection... FAILURE!')
+    # try:
+    #     print('DB connection... OK!')
+    # except:
+    #     print('DB connection... FAILURE!')
     print('BOT start... OK!')
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_start)
