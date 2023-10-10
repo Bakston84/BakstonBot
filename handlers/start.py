82ceeda2c93c2aa9c479625a1e5d96e26cafbd55
Bakston84 <Bakston1@yandex.ru>
2023-10-10 10:21:55 +0500

    101020231021

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,8 @@
 from aiogram.types import Message
 from aiogram.dispatcher import FSMContext
 from loader import dp
-from data import view
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: Message):
     tg_id = message.from_user.id
     await dp.bot.send_message(tg_id, text='Hi')
-    await view.name.set()
